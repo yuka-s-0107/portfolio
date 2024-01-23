acf873861d8644dea457a3ee18717c3db9cea753
yuka-s-0107 <y.shiraishi.0107@gmail.com>
2024-01-23 22:02:56 +0900

    レッスン後更新

--- src/Calendar.js
+++ src/Calendar.js
@@ -2,12 +2,18 @@ import React, { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
+import listPlugin from "@fullcalendar/list";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
+import { IconButton } from "@mui/material";
+import Textarea from "@mui/joy/Textarea";
+import { v4 as uuidv4 } from "uuid";
 
 let maxId = 0;
 const style = {
@@ -25,13 +31,20 @@ const style = {
 export default function Calendar() {
   const [open, setOpen] = React.useState(false); //日付欄を開ける（open）
   const [dataStr, setDataStr] = React.useState(""); //日付の取得（dataStr）
+  const [isEdit, setIsEdit] = React.useState(false); //編集か否か
+  const [id, setId] = React.useState("");
 
+  //新規登録画面
   const handleOpen = (arg) => {
     setDataStr(arg.dateStr);
+    setIsEdit(false);
     setOpen(true);
   };
 
-  const handleClose = () => setOpen(false);
+  const handleClose = () => {
+    setEvent("");
+    setOpen(false);
+  }; //外側を押したらmodalを閉じる
 
   const [event, setEvent] = useState(""); //入力値（event）
   const [eventList, setEventList] = useState([]); //予定リスト（eventList）
@@ -41,19 +54,47 @@ export default function Calendar() {
   };
 
   const handleDateClick = () => {
-    setEventList((list) => [
-      ...list,
-      { id: ++maxId, title: event, date: `${dataStr}` },
-    ]);
+    if (isEdit) {
+      console.log("id:", id);
+      setEventList((list) =>
+        list.map((e) => (e.id === id ? { ...e, title: event } : e))
+      );
+    } else {
+      setEventList((list) => [
+        ...list,
+        { id: uuidv4(), title: event, date: `${dataStr}` },
+      ]);
+    }
+
+    setEvent(""); //textareaを空にする
+    setOpen(false); //modalを閉じる
+  };
+
+  //編集画面
+  const handleEventClick = (info) => {
+    console.log(info.event.title);
+    console.log(info.event.date);
+    console.log(info.event.id);
+
+    setEvent(info.event.title);
+    setId(info.event.id);
+    setIsEdit(true);
+    setOpen(true);
   };
 
   return (
     <>
       <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin, listPlugin]}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,listWeek",
+        }}
         initialView="dayGridMonth"
         dateClick={handleOpen}
         events={eventList}
+        eventClick={handleEventClick}
       />
 
       <Modal
@@ -69,19 +110,21 @@ export default function Calendar() {
           {/* <Typography id="modal-modal-description" sx={{ mt: 2 }}>
             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
           </Typography> */}
-          <TextField
-            id="standard-basic"
-            label="schedule"
-            variant="standard"
-            input
-            type="text"
-            value={event}
-            onChange={handleChangeEvent}
-          />
+
+          <Textarea minRows={2} value={event} onChange={handleChangeEvent} />
+
           <div>
             <Button variant="text" type="button" onClick={handleDateClick}>
               save
             </Button>
+
+            {/* <Stack direction="row" spacing={1}> */}
+
+            <IconButton>
+              <DeleteIcon />
+            </IconButton>
+
+            {/* </Stack> */}
           </div>
         </Box>
       </Modal>
